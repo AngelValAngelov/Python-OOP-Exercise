57a707800fa83af80e2b19c87ef27f1c5df367f9
Angel <angelvalangelov@gmail.com>
2022-11-06 00:46:45 +0200

    List

--- testing_lab/list.py
+++ testing_lab/list.py
@@ -1,33 +1,3 @@
-# class IntegerList:
-#     def __init__(self, *args):
-#         self.list = [number for number in args]
-#
-#     def add(self, number):
-#         if isinstance(number, int):
-#             return self.list.append(number)
-#         raise ValueError
-#
-#     def remove_index(self, number):
-#         if isinstance(number, int):
-#             self.list.remove(number)
-#         raise IndexError
-#
-#     def get(self, number):
-#         if len(self.list) < number:
-#             return self.list[number]
-#         raise IndexError
-#
-#     def insert(self, number):
-#         if len(self.list) >= number:
-#             raise IndexError
-#         if not isinstance(number, int):
-#             raise ValueError
-#
-#     def get_biggest(self):
-#         return max(self.list)
-#
-#     def get_index(self, number):
-#         return self.list.index(number)
 class IntegerList:
     def __init__(self, *args):
         self.__data = []
@@ -89,9 +59,56 @@ class IntegerListTest(unittest.TestCase):
         self.assertEqual("Element is not Integer", str(ex.exception))
 
     def test_add_expect_add_integer_to_the_list(self):
-        self.integers.list = []
+        self.assertEqual(self.integers.add(1), [1])
+
+    def test_remove_expect_raise_error_if_the_element_is_bigger_than_length_of_the_list(self):
+        with self.assertRaises(IndexError) as ex:
+            self.integers.remove_index(5)
+        self.assertEqual("Index is out of range", str(ex.exception))
+
+    def test_remove_expect_remove_the_index_integer(self):
         self.integers.add(1)
-        self.assertEqual(self.integers.list, [1])
+        self.assertEqual(self.integers.remove_index(0), 1)
+
+    def test_get_integer_expect_raise_error_if_index_is_out_of_range(self):
+        with self.assertRaises(IndexError) as ex:
+            self.integers.get(10)
+        self.assertEqual("Index is out of range", str(ex.exception))
+
+    def test_get_integer_expect_return_correct_integer(self):
+        self.integers.add(2)
+        self.assertEqual(self.integers.get(0), 2)
+
+    def test_insert_integer_expect_raise_error_if_the_element_is_bigger_than_length_of_the_list(self):
+        self.integers.add(1)
+        with self.assertRaises(IndexError) as ex:
+            self.integers.insert(5, 1)
+        self.assertEqual("Index is out of range", str(ex.exception))
+
+    def test_insert_integer_expect_raise_error_if_integer_is_different_type(self):
+        self.integers.add(1)
+        with self.assertRaises(ValueError) as ex:
+            self.integers.insert(0, 1.1)
+        self.assertEqual("Element is not Integer", str(ex.exception))
+
+    def test_insert_integer_expect_insert_correctly_integer_to_the_integer_list(self):
+        self.integers.add(1)
+        self.integers.add(2)
+        self.integers.add(3)
+        self.integers.insert(0, 11)
+        self.assertEqual(self.integers.get_data(), [11, 1, 2, 3])
+
+    def test_get_biggest_expect_return_biggest_integer_of_the_integer_list(self):
+        self.integers.add(1)
+        self.integers.add(2)
+        self.integers.add(3)
+        self.assertEqual(self.integers.get_biggest(), 3)
+
+    def test_get_index_expect_return_correct_index_of_the_integer(self):
+        self.integers.add(1)
+        self.integers.add(2)
+        self.integers.add(3)
+        self.assertEqual(self.integers.get_index(2), 1)
 
 
 if __name__ == '__main__':
